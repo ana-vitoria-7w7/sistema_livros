fe07de57329aa53a664c5b0fa1c5401b454af058
ana-vitoria-7w7 <166128639+ana-vitoria-7w7@users.noreply.github.com>
2025-04-22 16:50:35 -0300

    Update formRecomendacoes.php
    Troque os selects por inputs normais, estava com dificuldades em puxar os dados das tabelas

--- Views/Recomendacoes/formRecomendacoes.php
+++ Views/Recomendacoes/formRecomendacoes.php
@@ -4,18 +4,10 @@
     <form action="index.php?page=cadastrarRecomendacoes" method="POST">
         <p>Fazer uma nova recomendação!</p><br><br>
         <label for="usuario">Usuário:</label>
-        <select id="usuario" name="usuarioId" required>
-            <?php foreach ($usuarios as $usuario): ?>
-                <option value="<?= $usuario['id'] ?>"><?= $usuario['nome'] ?></option>
-            <?php endforeach; ?>
-        </select><br>
+        <input type="text" placeholder="Digite seu ID de usuário" id="usuario" name="usuario_id" required><br><br>
 
         <label for="genero">Gênero:</label>
-        <select id="genero" name="generoId" required>
-            <?php foreach ($generos as $genero): ?>
-                <option value="<?= $genero['id'] ?>"><?= $genero['nome'] ?></option>
-            <?php endforeach; ?>
-        </select><br>
+        <input type="text" placeholder="Digite o ID do gênero" id="genero" name="genero_id" required><br><br>
 
         <label for="livro">Livro Recomendado:</label>
         <input type="text" id="livro" name="livroRecomendado" required><br><br>
