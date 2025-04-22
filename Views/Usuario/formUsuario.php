298b4669adbda014727c89071bbfbb2251979cf1
ana-vitoria-7w7 <166128639+ana-vitoria-7w7@users.noreply.github.com>
2025-04-22 17:01:04 -0300

    Update formUsuario.php

--- Views/Usuario/formUsuario.php
+++ Views/Usuario/formUsuario.php
@@ -4,10 +4,10 @@
     <form action="index.php?page=cadastrarUsuario" method="POST">
         <p> Cadastre seu usuário!</p><br><br>
         <label for="nome">Nome:</label>
-        <input class="tamanhoCampo" type="text" id="nome" name="nome" required><br><br>
+        <input class="tamanhoCampo" type="text" placeholder="Digite o seu nome" id="nome" name="nome" required><br><br>
 
         <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" required><br><br><br>
+        <input class="tamanhoCampo" type="email" placeholder="Digite o seu email" id="email" name="email" required><br><br><br>
 
         <button type="submit">Cadastrar</button>
     </form>
